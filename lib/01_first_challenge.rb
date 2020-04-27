407004d6ba64ac92f293a4833860ec449d4a7477
Leonora Squires <leonora.squires@gmail.com>
2020-04-27 06:01:41 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,22 +1,6 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {
-       :patriach => {:name => "Lord Montague", :age => "52"}, 
-       :matriarch => {:name => "Lady Montague", :age => "54"}, 
-       :hero => {:name => "Romeo", :age => "15", :status => "alive"}, 
-       :hero_friends => [
-          {:name => "Benvolio", :age => 17, :attitude => "worried"},
-          {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
-        ]
-     },
-     :capulet => {
-       :patriarch => {:name =>"Lord Capulet", :age => "50"}, 
-       :matriarch => {:name => "Lady Capulet", :age => "51"}, 
-       :heroine => {:name => "Juliet", :age => "15", :status => "alive"}, 
-       :heroine_friends => [
-          {:name => "Steve", :age => "30", :attitude => "confused"},
-          {:name => "Nurse", :age => "44", :attitude => "worried"}
-          ]
-     }
-   }
+epic_tragedy = {
+  :montague => {},
+  :capulet => {}
+}
 end
