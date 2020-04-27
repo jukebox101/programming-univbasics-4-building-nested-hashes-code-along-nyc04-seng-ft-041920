9c6fbdaac950fcf00811ce99c86967af93f20235
Leonora Squires <leonora.squires@gmail.com>
2020-04-27 04:58:04 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,14 +2,18 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+        :name =>"Lord Capulet", :
+        age => "50"
+      }, 
+       :matriarch => {
+         :name => "Lady Capulet", 
+         :age => "51"
+       }, 
+       :heroine => {
+         :name => "Juliet", 
+         :age => "15", 
+         :status => "alive"
+       }, 
       :hero_friends => []
    }, 
    :capulet => {
